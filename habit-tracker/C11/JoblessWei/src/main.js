46e17cdfecf0ac0da5d6fa874d60bb38ecf193bf
JoblessWei <weic022405@gmail.com>
2025-08-18 21:53:49 -0400

    added edit habits functionality

--- habit-tracker/C11/JoblessWei/src/main.js
+++ habit-tracker/C11/JoblessWei/src/main.js
@@ -1,7 +1,9 @@
 const habits = []
-
+let editIndex = -1
 // show modal when create new habit button is clicked
 document.getElementById("create_habit_btn").addEventListener("click", () =>{
+    editIndex = -1
+    document.getElementById('habitForm').reset()
     document.getElementById("habitModal").classList.remove("hidden")
 });
 
@@ -9,18 +11,24 @@ document.getElementById("create_habit_btn").addEventListener("click", () =>{
 // handle form submissions
 document.getElementById('habitForm').addEventListener('submit', function(e) {
     e.preventDefault(); 
-    console.log(e)
     const formData = new FormData(e.target);
     const habit = {
         name: formData.get('habitName'),
         category: formData.get('category'),
         frequency: formData.get('frequency'),
         reminder: formData.get('reminder') === 'on',
-        streak: 0
+        streak: editIndex >= 0 ? habits[editIndex].streak : 0
     };
 
     try{
-        habits.push(habit)
+        if (editIndex >= 0){
+            habits[editIndex] = habit     // update existing habit (edit function)
+        }
+        else{
+            habits.push(habit)       //  create a new one
+            console.log('New habit created:', habit);
+        }
+
         localStorage.setItem("habits", JSON.stringify(habits))  // add habit to list
 
     } catch (error){
@@ -28,26 +36,29 @@ document.getElementById('habitForm').addEventListener('submit', function(e) {
     }
     document.getElementById("habitModal").classList.add("hidden")
     renderHabit(habits)
-    console.log('New habit created:', habit);
 
+
+    editIndex = -1
 });
 
 const renderHabit = (habits) => {
     const habitList = document.getElementById("habit_list")
-
+    
     habitList.innerHTML = habits.map((habit, index) => {
         return  `<div class="habitCard">
         <div class="cardContent">
             <p>${habit.name}</p>
             <p>Category: ${habit.category}</p>
             <p>Streak:${habit.streak}</p>
-            <p>Frquency: ${habit.frequency}</p>
+            <p>Frequency: ${habit.frequency}</p>
         </div>
         <div class="cardButtons">
             <button class="deleteHabitButton" data-index=${index}> Delete Habit </button>
             <button class="completeButton" data-index=${index}> Mark as Complete </button>
+            <button class="editHabitButton" data-index=${index}> Edit </button>
         </div>
     </div>`
+    
     }).join('\n')
 
 }
@@ -57,12 +68,35 @@ document.addEventListener('click', (e) =>{
     if (e.target.classList.contains('completeButton')){
         const index = e.target.getAttribute('data-index')
 
-        console.log(index)
-        
         habits[index].streak += 1;
         renderHabit(habits)
-        console.log(habits[index].streak)
         localStorage.setItem("habits", JSON.stringify(habits))
     }
+    // delete habit when button is pressed
+    if (e.target.classList.contains('deleteHabitButton')){
+    const index = e.target.getAttribute('data-index')
 
+    habits.splice(index, 1)
+    renderHabit(habits)
+    localStorage.setItem("habits", JSON.stringify(habits))
+
+    }
+
+    // edit habit if edit button is pressed
+    if(e.target.classList.contains('editHabitButton')){
+        const index = parseInt(e.target.getAttribute('data-index'))
+        const habit = habits[index]
+
+        editIndex = index
+        document.getElementById("habitModal").classList.remove("hidden")
+        
+        document.getElementById('habitName').value = habit.name;
+        document.getElementById('category').value = habit.category;
+        document.getElementById('frequency').value = habit.frequency;
+        document.getElementById('reminder').checked = habit.reminder;
+
+
+    }
 })
+
+
